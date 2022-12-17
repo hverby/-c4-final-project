12d73ec8b6774ae29c2968491a93e5c0d1f851bf
Hans-Vernier <hansbegou@gmail.com>
2022-12-17 04:39:27 +0000

    refact auth

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,10 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify } from 'jsonwebtoken'
+import { verify, decode} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -12,8 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-//const jwkSUrl = "https://dev-6ziekzilpww5d5pt.us.auth0.com/.well-known/jwks.json"
-const secretId = process.env.AUTH_0_SECRET_ID
+const jwkSUrl = "https://dev-6ziekzilpww5d5pt.us.auth0.com/.well-known/jwks.json"
 
 
 export const handler = async (
@@ -58,12 +57,27 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token: string = getToken(authHeader)
-  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
 
-  return verify(token, secretId) as Promise<JwtPayload>;
+  const response = await Axios.get(jwkSUrl)
+  const keys = response.data.keys
+  const signingKeys = keys.find(key => key.kid === jwt.header.kid)
+  logger.info('signingKeys', signingKeys)
+  if (!signingKeys) {
+  throw new Error ('The JWKS endpoint did not contain any keys')
+  }
+    // get pem data
+    const pemData = signingKeys.x5c[0]
+    // convert pem data to cert
+    const cert =`---BEGIN CERTIFICATE---\n${pemData}\n----END CERTIFICATE---`
+
+    // verify token
+    const verifiedToken = verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+    logger.info('verifiedToken', verifiedToken)
+    return verifiedToken
 }
 
 function getToken(authHeader: string): string {
